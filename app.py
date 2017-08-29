e0e0c1d33572b1127cf4aebd2ae171c23f993b4b
Federico Ulfo <federicoulfo@gmail.com>
2017-08-29 10:48:03 -0400

    Added POST /users API and removed GET /user/<user_id>

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_restful import Api
 from conf.config import HTTP_HOST, HTTP_PORT
 
 from service.create_user import CreateUser
-from service.get_user import GetUser
+from service.get_user import GetUsers
 from service.healthz import Healthz
 from service.me import Me
 from service.update_user import UpdateUser
@@ -14,10 +14,10 @@ app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Healthz, '/healthz')
-api.add_resource(CreateUser, '/user')
-api.add_resource(GetUser, '/user/<user_id>')
-api.add_resource(UpdateUser, '/user')
 api.add_resource(Me, '/me')
+api.add_resource(CreateUser, '/user')
+api.add_resource(UpdateUser, '/user')
+api.add_resource(GetUsers, '/users')
 
 if __name__ == '__main__':
     app.run(host=HTTP_HOST, port=HTTP_PORT)
--- service/get_user.py
+++ service/get_user.py
@@ -1,16 +1,29 @@
 from cassandra.cqlengine import connection
-from flask import make_response
+from flask import make_response, request
 from flask_restful import Resource
 
 from conf.config import CASSANDRA_HOSTS, USER_KEYSPACE
 from model.user import UserInfoById
 
 
-class GetUser(Resource):
-    def get(self, user_id):
-        connection.setup(hosts=CASSANDRA_HOSTS, default_keyspace=USER_KEYSPACE)
-        user = UserInfoById.filter(user_id=user_id)
-        if not user:
-            return make_response("User not found", 404)
+class GetUsers(Resource):
+    @staticmethod
+    def post():
 
-        return user.get().to_object()
+        data = request.get_json(silent=True)
+        user_ids = data.get("user_ids")
+        if not user_ids:
+            return make_response("You must send the user_ids parameter, formatted as a list of user_id", 405)
+
+        connection.setup(hosts=CASSANDRA_HOSTS, default_keyspace=USER_KEYSPACE)
+
+        users = UserInfoById.filter(user_id__in=user_ids)
+
+        user_list = []
+        for userfriend_row in users:
+            user_list.append(userfriend_row.to_object())
+
+        return {
+            "results": users.count(),
+            "users": user_list
+        }
